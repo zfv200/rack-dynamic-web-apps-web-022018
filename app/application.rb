64ca6a72cfeec33da9eeda123535260185ba15ef
github username <github email address>
2018-03-10 18:14:17 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -7,14 +7,10 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
 
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
-
     if num_1==num_2 && num_2==num_3
-      resp.write "You win"
+      resp.write "You Win"
     else
-      resp.write "You lose"
+      resp.write "You Lose"
     end
 
     resp.finish
